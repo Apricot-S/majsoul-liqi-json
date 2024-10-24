030ba6da13dd4a2e5c6b9fd10f051ad14bc4dd10
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-24 14:39:29 +0000

    chore: Use `uv sync` instead of `uv pip`.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -26,8 +26,5 @@ uv python install $PYTHON_VERSION --no-progress
 
 pushd /workspaces/majsoul-liqi-json
 rm -rf .venv
-uv venv
-uv pip install -U \
-  jsonschema \
-  types-jsonschema
+uv sync
 popd
